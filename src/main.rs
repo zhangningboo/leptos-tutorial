df0bfc0d1d258e605cc0fb264519d07aaea88351
zhangningboo <zhangningbo21@mails.ucas.ac.cn>
2025-09-13 18:58:16 +0800

    feat: 3.4. Iteration

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,55 @@ use leptos::prelude::*;
 
 mod components;
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+struct Counter {
+  id: usize,
+  count: RwSignal<i32>
+}
+
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
     let double_count = move || count.get() * 2;
     let html = "<p>This HTML will be injected.</p>";
 
+    let values = vec![0, 1, 2];
+    
+    let length = 5;
+    let _counters: Vec<RwSignal<Counter>> = (1..=length).map(|idx| RwSignal::new(Counter { id: idx, count: RwSignal::new(0) })).collect::<Vec<_>>();
+    let (counters, _set_counters) = signal::<Vec<Counter>>(vec![Counter { id: 0, count: RwSignal::new(0) }, Counter { id: 0, count: RwSignal::new(0) }]);
     use crate::components::progress_bar::ProgressBar;
     view! {
-        <div inner_html=html/>
+        <div inner_html=html/> 
+
+        <ForEnumerate
+            each=move || counters.get() // Same as <For/>
+            key=|counter| counter.id    // Same as <For/>
+            // Provides the index as a signal and the child T
+            children={move |index: ReadSignal<usize>, counter: Counter| {
+                view! {
+                    <button
+                        on:click=move |_| { *counter.count.write() += 1; }
+                        class="px-5 py-3 bg-sky-600 hover:bg-sky-700 text-white rounded-lg"
+                    >{move || index.get()} ". Value: " {move || counter.count.get()}</button>
+                    <br />
+                }
+            }}
+        />
+
+        <p>{values.clone()}</p>
+        // or we can wrap them in <li>
+        <ul>
+            {values.clone().into_iter()
+                .map(|n| view! { <li>{n}</li>})
+                .collect::<Vec<_>>()}
+        </ul>
+
+        <ul>
+            {values.clone().into_iter()
+                .map(|n| view! { <li>{n}</li>})
+                .collect_view()}
+        </ul>
 
         <button
             on:click=move |_| { *set_count.write() += 1; }
